f7c705441a89a780fcbbe3e3c277d54217d445a5
Ritik <varribhar@gmail.com>
2022-07-28 09:08:14 +0530

    added JOIN

--- client/src/Pages/JoinCreateTeam.jsx
+++ client/src/Pages/JoinCreateTeam.jsx
@@ -4,18 +4,22 @@ const JoinCreateTeam = () => {
   return (
     <div>
       <div>
-      <div className='mx-auto rounded flex border border-black w-4/5 h-[500px] p-10 gap-2'>
-        <div className='w-1/2 border border-red-500 rounded h-full text-center '>
+      <div className=' mt-10 mx-auto rounded flex  w-4/5 h-[500px] p-10 gap-5 bg-orange-200'>
+        <div className='w-1/2  h-full text-center bg-white rounded pt-5'>
             <h2 className='text-2xl mb-5'>Create Team</h2>
-            <input className='rounded-full border border-teal-900 text-white px-2 py-1 'type="text" placeholder='Enter Team ID'/>
-            <button className='rounded-full bg-teal-600 text-white px-2 py-1'>Create Team</button>
+            <div className='flex flex-col items-center gap-5'>
+                <input className='rounded-full border border-teal-900 px-2 py-1'type="text" placeholder='Enter Team Name'/>
+                {/* <input className='rounded-full border border-teal-900 px-2 py-1'type="text" placeholder='Enter Team Name'/> */}
+                <button className='rounded-full bg-teal-600 text-white px-2 py-1'>Create Team</button>
+            </div>
+            
         </div>
-        <div className='border border-teal-900 rounded-full'></div>
-        <div className='w-1/2 border border-red-500 rounded h-full text-center'>
+        <div className='border border-teal-50 rounded-full'></div>
+        <div className='w-1/2  rounded h-full text-center bg-white pt-5'>
             <h2 className='text-2xl mb-5'>Join Team </h2>
             
-            <div className='flex justify-center gap-5'>
-                <input className='rounded-full border border-teal-900 text-white px-2 py-1 'type="text" placeholder='Enter Team ID'/>
+            <div className='flex justify-center gap-5 '>
+                <input className='rounded-full border border-teal-900  px-2 py-1 'type="text" placeholder='Enter Team ID'/>
                 <button className='rounded-full bg-teal-600 text-white px-2 py-1'>Join Team</button>
             
             </div>
